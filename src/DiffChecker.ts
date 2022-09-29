01b93917a81206022d9f9ebb181df64262495150
Tal Lannder <tal@pjili.com>
2022-09-29 05:31:42 -0400

    feat: check only lines on minIncrease

--- src/DiffChecker.ts
+++ src/DiffChecker.ts
@@ -71,8 +71,8 @@ export class DiffChecker {
   checkIfTestCoverageShouldIncrease(minimum: number, delta: number): boolean {
     const file = 'total'
     const diffCoverageData = this.diffCoverageReport[file]
-    const keys: ('lines' | 'statements' | 'branches' | 'functions')[] = <
-      ('lines' | 'statements' | 'branches' | 'functions')[]
+    const keys: ('lines')[] = <
+      ('lines')[]
     >Object.keys(diffCoverageData)
     const keyResults = keys.map(key => {
       const oldPct = diffCoverageData[key].oldPct ?? 0
